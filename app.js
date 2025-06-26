d6a4885532e5bdb5ada30f2a4383a8bad569b843
KA-rg <krishjain260905@gmail.com>
2025-06-26 22:34:51 +0530

    js modified for add button

---
+++ app.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function() {
+  console.log("button was pessed");
+});
