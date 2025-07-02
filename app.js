573a829a5a049912a6e2813a3554e50223ff9915
KA-rg <krishjain260905@gmail.com>
2025-07-02 23:30:41 +0530

    adding delete btn

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let btn = document.querySelector("button");
 let inp = document.querySelector("input");
 let ul = document.querySelector("ul");
+let del = document.querySelector(".del");
 
 btn.addEventListener("click", function() {
   let item = document.createElement("li");
-  item.innerText = inp.value;
+  item.innerHTML = `<li>${inp.value} <button>Delete</button></li>`;
   ul.appendChild(item);
   inp.value = "";
+});
+
+del.addEventListener("click", function () {
+  
 });
